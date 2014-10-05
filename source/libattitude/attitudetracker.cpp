b8595a91edcc99eb97eba9ab582caf7a46c1a296
Martin Stoilov <martin@rpasearch.com>
2014-10-04 23:24:52 -0700

    removed temporary quaternion decomposition.

--- source/libattitude/attitudetracker.cpp
+++ source/libattitude/attitudetracker.cpp
@@ -32,25 +32,6 @@ void attitudetracker::set_nlerp_blend(double blend_g)
 	blend_g_ = blend_g;
 }
 
-/*
- * composite_rotation = swing * twist
- */
-static void swing_twist_decomposition(
-		const QuaternionD& rotation,
-		const Vector3d& direction,
-		QuaternionD& swing,
-		QuaternionD& twist)
-{
-	Vector3d rotation_axis(rotation.x, rotation.y, rotation.z);
-	// return projection v1 on to v2 (parallel component)
-	// here can be optimized if direction is unit
-	Vector3d proj = Vector3d::projection(rotation_axis, direction);
-
-	twist = QuaternionD(rotation.w, proj.at(0, 0), proj.at(1, 0), proj.at(2, 0));
-	twist.normalize();
-	swing = rotation * twist.conjugated();
-}
-
 void attitudetracker::track_gyroscope(Vector3d omega, double dtime)
 {
 	QuaternionD deltaq = QuaternionD::fromAngularVelocity(omega, dtime);
