fb7ba6175fc9ece3591f5269884aced64b56e313
Martin Stoilov <martin@rpasearch.com>
2014-10-18 21:48:52 -0700

    added support for HTTP headers.

--- source/libjsonspirit/rpcclient.cpp
+++ source/libjsonspirit/rpcclient.cpp
@@ -1,8 +1,9 @@
 #include "rpcclient.h"
 
 
-rpc_client::rpc_client(std::string address, std::string port, size_t timeout)
+rpc_client::rpc_client(std::string address, std::string port, size_t timeout, const std::string& jsonrpc_version)
 	: http::client::http_client(address, port, timeout)
+	, jsonrpc_version_(jsonrpc_version)
 	, serial_(0)
 	, failures_(0)
 {
@@ -17,22 +18,18 @@ size_t rpc_client::get_failure_count()
 	return failures_;
 }
 
-json::value rpc_client::call(const std::string& url, const std::string& method) throw(std::exception)
-{
-	json::array params;
-	return call(url, method, params);
-}
-
-json::value rpc_client::call(const std::string& url, const std::string& method, const json::array& params) throw(std::exception)
+json::value rpc_client::call(const std::string& url, const std::string& method, const json::array& params, http::headers headers) throw(std::exception)
 {
 	json::value rpc_response;
 	json::object rpc_request;
 	json::object rpc_error;
 	http::client::response response;
-	http::headers headers;
 
-	headers.insert_header("Connection", "keep-alive");
-	rpc_request["jsonrpc"] = "1.0";
+	if (headers.find_header("Connection").empty())
+		headers.insert_header("Connection", "keep-alive");
+	if (headers.find_header("content-type").empty())
+		headers.insert_header("content-type", "application/json-rpc");
+	rpc_request["jsonrpc"] = jsonrpc_version_;
 	rpc_request["id"] = (int)++serial_;
 	rpc_request["method"] = json::value(method);
 	rpc_request["params"] = params;
@@ -45,14 +42,20 @@ json::value rpc_client::call(const std::string& url, const std::string& method, 
 	return rpc_response.get_obj()["result"];
 }
 
-json::value rpc_client::call(const std::string& url, const std::string& method, const json::value& val1) throw(std::exception)
+json::value rpc_client::call(const std::string& url, const std::string& method, http::headers headers) throw(std::exception)
+{
+	json::array params;
+	return call(url, method, params, headers);
+}
+
+json::value rpc_client::call(const std::string& url, const std::string& method, const json::value& val1, http::headers headers) throw(std::exception)
 {
 	json::array params;
 	params.push_back(val1);
 	return call(url, method, params);
 }
 
-json::value rpc_client::call(const std::string& url, const std::string& method, const json::value& val1, const json::value& val2) throw(std::exception)
+json::value rpc_client::call(const std::string& url, const std::string& method, const json::value& val1, const json::value& val2, http::headers headers) throw(std::exception)
 {
 	json::array params;
 	params.push_back(val1);
@@ -60,7 +63,13 @@ json::value rpc_client::call(const std::string& url, const std::string& method, 
 	return call(url, method, params);
 }
 
-json::value rpc_client::call(const std::string& url, const std::string& method, const json::value& val1, const json::value& val2, const json::value& val3) throw(std::exception)
+json::value rpc_client::call(
+		const std::string& url,
+		const std::string& method,
+		const json::value& val1,
+		const json::value& val2,
+		const json::value& val3,
+		http::headers headers) throw(std::exception)
 {
 	json::array params;
 	params.push_back(val1);
--- source/libjsonspirit/rpcclient.h
+++ source/libjsonspirit/rpcclient.h
@@ -9,17 +9,39 @@
 class rpc_client : public http::client::http_client
 {
 public:
-	rpc_client(std::string address, std::string port, size_t timeout = 0);
+	rpc_client(std::string address, std::string port, size_t timeout = 0, const std::string& jsonrpc_version = "1.0");
 	~rpc_client();
-	json::value call(const std::string& url, const std::string& method, const json::array& params) throw(std::exception);
-	json::value call(const std::string& url, const std::string& method) throw(std::exception);
-	json::value call(const std::string& url, const std::string& method, const json::value& val1) throw(std::exception);
-	json::value call(const std::string& url, const std::string& method, const json::value& val1, const json::value& val2) throw(std::exception);
-	json::value call(const std::string& url, const std::string& method, const json::value& val1, const json::value& val2, const json::value& val3) throw(std::exception);
+	json::value call(const std::string& url,
+			const std::string& method,
+			const json::array& params,
+			http::headers headers = http::headers()) throw(std::exception);
+
+	json::value call(const std::string& url,
+			const std::string& method,
+			http::headers headers = http::headers()) throw(std::exception);
+
+	json::value call(const std::string& url,
+			const std::string& method,
+			const json::value& val1,
+			http::headers headers = http::headers()) throw(std::exception);
+
+	json::value call(const std::string& url,
+			const std::string& method,
+			const json::value& val1,
+			const json::value& val2,
+			http::headers headers = http::headers()) throw(std::exception);
+
+	json::value call(const std::string& url,
+			const std::string& method,
+			const json::value& val1,
+			const json::value& val2,
+			const json::value& val3,
+			http::headers headers = http::headers()) throw(std::exception);
 
 	size_t get_failure_count();
 
 protected:
+	std::string jsonrpc_version_;
 	size_t serial_;
 	size_t failures_;
 };
