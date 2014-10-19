85ec2dc3c00bc3a5cee24e685fc5f3b5f3d72442
Martin Stoilov <martin@rpasearch.com>
2014-10-18 21:51:03 -0700

    better error reporting

--- source/tools/flightdeck/mainwindow.cpp
+++ source/tools/flightdeck/mainwindow.cpp
@@ -4,7 +4,7 @@
 mainwindow::mainwindow(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade)
 	: Gtk::Window(cobject)
 	, timer_number_(0)
-	, rpcuri_("/jsonrpc/")
+	, rpcuri_("/jsonrpc")
 	, rpc_client_(new rpc_client("localhost", "18222"))
 	, ref_glade_(refGlade)
 	, button_quit_(NULL)
@@ -96,7 +96,7 @@ void mainwindow::on_change_thrust()
 		rpc_client_->call(rpcuri_, "sd_set_thrust", spinbutton_thrust_->get_value());
 		rpc_update_thrust();
 	} catch (std::exception& e) {
-		std::cout << "on_change_g exception: " << e.what() << std::endl;
+		std::cout << "on_change_thrust exception: " << e.what() << std::endl;
 	}
 }
 
@@ -116,51 +116,72 @@ void mainwindow::set_rpc_connection(const std::string& rpcserver, const std::str
 {
 	try {
 		timer_conn_.disconnect();
-		rpc_client_.reset(new rpc_client(rpcserver, rpcport, 30000));
+		rpc_client_.reset(new rpc_client(rpcserver, rpcport, 30000, "2.0"));
 		on_rpc_update();
 		sigc::slot<bool> my_slot = sigc::mem_fun(*this, &mainwindow::on_rpc_update);
 		timer_conn_ = Glib::signal_timeout().connect(my_slot, 1000/(updaterate + 1));
 	} catch (std::exception& e) {
-		std::cout << "on_change_g exception: " << e.what() << std::endl;
+		std::cout << "set_rpc_connection exception: " << e.what() << std::endl;
 	}
 }
 
 void mainwindow::rpc_update_attitude()
 {
-	QuaternionD q;
-	json::value val = rpc_client_->call(rpcuri_, "sd_get_attitude");
-	q = quaternion_from_json_value<double>(val);
-	label_attitude_w_->set_text(double_to_str(q.w));
-	label_attitude_x_->set_text(double_to_str(q.x));
-	label_attitude_y_->set_text(double_to_str(q.y));
-	label_attitude_z_->set_text(double_to_str(q.z));
+	try {
+		QuaternionD q;
+		json::value val = rpc_client_->call(rpcuri_, "sd_get_attitude");
+		q = quaternion_from_json_value<double>(val);
+		label_attitude_w_->set_text(double_to_str(q.w));
+		label_attitude_x_->set_text(double_to_str(q.x));
+		label_attitude_y_->set_text(double_to_str(q.y));
+		label_attitude_z_->set_text(double_to_str(q.z));
+	} catch (std::exception& e) {
+		std::cout << "rpc_update_attitude(sd_get_attitude) exception: " << e.what() << std::endl;
+	}
 }
 
 void mainwindow::rpc_update_motors()
 {
-	json::value val = rpc_client_->call(rpcuri_, "sd_get_motors");
-	label_m1_->set_text(double_to_str(val.get_array().at(0).get_real(), 3));
-	label_m2_->set_text(double_to_str(val.get_array().at(1).get_real(), 3));
-	label_m3_->set_text(double_to_str(val.get_array().at(2).get_real(), 3));
-	label_m4_->set_text(double_to_str(val.get_array().at(3).get_real(), 3));
+	try {
+		json::value val = rpc_client_->call(rpcuri_, "sd_get_motors");
+		label_m1_->set_text(double_to_str(val.get_array().at(0).get_real(), 3));
+		label_m2_->set_text(double_to_str(val.get_array().at(1).get_real(), 3));
+		label_m3_->set_text(double_to_str(val.get_array().at(2).get_real(), 3));
+		label_m4_->set_text(double_to_str(val.get_array().at(3).get_real(), 3));
+	} catch (std::exception& e) {
+		std::cout << "rpc_update_motors(sd_get_motors) exception: " << e.what() << std::endl;
+	}
 }
 
 void mainwindow::rpc_update_g()
 {
-	Vector3d G = matrix_from_json_value<double, 3, 1>(rpc_client_->call(rpcuri_, "sd_get_earth_g_vector"));
-	spinbutton_g_x_->set_value(G.at(0, 0));
-	spinbutton_g_y_->set_value(G.at(1, 0));
-	spinbutton_g_z_->set_value(G.at(2, 0));
+	try {
+		Vector3d G = matrix_from_json_value<double, 3, 1>(rpc_client_->call(rpcuri_, "sd_get_earth_g_vector"));
+		spinbutton_g_x_->set_value(G.at(0, 0));
+		spinbutton_g_y_->set_value(G.at(1, 0));
+		spinbutton_g_z_->set_value(G.at(2, 0));
+	} catch (std::exception& e) {
+		std::cout << "rpc_update_g(sd_get_earth_g_vector) exception: " << e.what() << std::endl;
+	}
 }
 
 void mainwindow::rpc_update_thrust()
 {
-	spinbutton_thrust_->set_value(rpc_client_->call(rpcuri_, "sd_get_thrust").get_real());
+	try {
+		spinbutton_thrust_->set_value(rpc_client_->call(rpcuri_, "sd_get_thrust").get_real());
+	} catch (std::exception& e) {
+		std::cout << "rpc_update_thrust(sd_get_thrust) exception: " << e.what() << std::endl;
+	}
 }
 
 void mainwindow::rpc_update_armed()
 {
-	button_arm_motors_->set_active(rpc_client_->call(rpcuri_, "sd_get_running").get_bool());
+	try {
+		button_arm_motors_->set_active(rpc_client_->call(rpcuri_, "sd_get_running").get_bool());
+	} catch (std::exception& e) {
+		std::cout << "rpc_update_armed(sd_get_running) exception: " << e.what() << std::endl;
+	}
+
 }
 
 bool mainwindow::on_rpc_update()
@@ -172,7 +193,7 @@ bool mainwindow::on_rpc_update()
 		rpc_update_g();
 		rpc_update_thrust();
 	} catch (std::exception& e) {
-		std::cout << "rpc_update_attitude exception: " << e.what() << std::endl;
+		std::cout << "on_rpc_update exception: " << e.what() << std::endl;
 	}
 	return true;
 }
