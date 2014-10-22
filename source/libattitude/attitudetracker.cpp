36d397bc43b0b89ec3875f74721fc595a79f04e3
Martin Stoilov <martin@rpasearch.com>
2014-10-21 23:25:47 -0700

    work on magnetometer tracking

--- source/libattitude/attitudetracker.cpp
+++ source/libattitude/attitudetracker.cpp
@@ -77,36 +77,10 @@ void attitudetracker::track_magnetometer(const Vector3d& m)
 {
 	g2m_q_ = QuaternionD::fromVectors(attitude_.rotate(earth_g_.normalize()), m.normalize());
 
-	Vector3d m_estimated = attitude_.rotate(earth_m_.normalize());
+	Vector3d m_estimated = attitude_.rotate(get_earth_m().normalize());
 	QuaternionD q = QuaternionD::fromVectors(m_estimated, m.normalize());
-	QuaternionD deltaq = QuaternionD::nlerp(QuaternionD::identity, q, blend_g_/2.0);
-
-#if 1
-	QuaternionD g_estimated(attitude_.rotate(earth_g_.normalize()), 0.0);
-	QuaternionD qin(deltaq.axis(), 0.0);
-	QuaternionD qper = (qin - g_estimated * qin * g_estimated) * 1.0 / 2.0;
-	deltaq = QuaternionD::fromAxisRot(qper.axis(), deltaq.angle());
-#endif
-
-	attitude_ = (deltaq.normalize() * attitude_).normalize();
-
-
-#if 0
-	/*
-	 * Earth plane represented by its normal vector
-	 */
-	QuaternionD earth_plane_q(attitude_.rotate(earth_g_.normalize()));
-
-	/*
-	 * Magnetic field as Quaternion
-	 */
-	QuaternionD magnetic_field_q(magnetic_field.normalize());
-	QuaternionD magnetic_field_estimated_q = QuaternionD(attitude_.rotate(Vector3d(0.06, -0.194, -0.978).normalize()));
-	QuaternionD magnetic_field_proj_q = (magnetic_field_q + earth_plane_q * magnetic_field_q * earth_plane_q) * 1.0 / 2.0;
-	QuaternionD magnetic_field_estimated_proj_q = (magnetic_field_estimated_q + earth_plane_q * magnetic_field_estimated_q * earth_plane_q) * 1.0 / 2.0;
-	Vector3d m_estimated(magnetic_field_estimated_proj_q.x, magnetic_field_estimated_proj_q.y, magnetic_field_estimated_proj_q.z);
-	Vector3d m(magnetic_field_proj_q.x, magnetic_field_proj_q.y, magnetic_field_proj_q.z);
-#endif
+	QuaternionD deltaq = QuaternionD::nlerp(QuaternionD::identity, q, blend_g_/3.0);
+//	attitude_ = (deltaq.normalize() * attitude_).normalize();
 }
 
 QuaternionD attitudetracker::get_attitude() const
