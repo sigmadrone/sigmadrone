bf86a30a8bbab7f6d55bf9baf29554f399f64b3d
Martin Stoilov <martin@rpasearch.com>
2014-12-21 18:12:38 -0800

    added cortex-m4

---
+++ cortex-m4/test-printf/application/digitalin.cpp
@@ -0,0 +1,121 @@
+/*
+ * digitalin.cpp
+ *
+ *  Created on: Dec 10, 2014
+ *      Author: mstoilov
+ */
+
+
+//#include "stm32f4xx_hal_cortex.h"
+#include "../application/digitalin.h"
+
+static DigitalIn* g_interrupt[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
+
+void DigitalIn::vector_handler(uint8_t line)
+{
+	uint16_t GPIO_Pin = 1 << line;
+	if (__HAL_GPIO_EXTI_GET_IT(GPIO_Pin) != RESET) {
+		__HAL_GPIO_EXTI_CLEAR_IT(GPIO_Pin);
+		if (g_interrupt[line])
+			g_interrupt[line]->callback_.call();
+	}
+}
+
+DigitalIn::DigitalIn(PinName pin, PullMode pmode, InterruptMode imode)
+	: pin_(STM_PIN(pin))
+	, GPIOx_((GPIO_TypeDef *) (GPIOA_BASE + 0x0400 * STM_PORT(pin)))
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	clock_enable(STM_PORT(pin));
+	GPIO_InitStructure.Pin = 1 << pin_;
+	if (imode == InterruptRising)
+		GPIO_InitStructure.Mode = GPIO_MODE_IT_RISING;
+	else if (imode == InterruptRising)
+		GPIO_InitStructure.Mode = GPIO_MODE_IT_FALLING;
+	else if (imode == InterruptRisingFalling)
+		GPIO_InitStructure.Mode = GPIO_MODE_IT_RISING_FALLING;
+	else
+		GPIO_InitStructure.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStructure.Speed = GPIO_SPEED_FAST;
+	if (pmode == PullUp)
+		GPIO_InitStructure.Pull = GPIO_PULLUP;
+	else if (pmode == PullDown)
+		GPIO_InitStructure.Pull = GPIO_PULLDOWN;
+	else
+		GPIO_InitStructure.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOx_, &GPIO_InitStructure);
+
+	/* Enable and set EXTI Line0 Interrupt to the lowest priority */
+	if (imode != InterruptNone && pin_ < 16) {
+		g_interrupt[pin_] = this;
+		switch (pin_) {
+		case 0:
+			HAL_NVIC_SetPriority(EXTI0_IRQn, 2, 0);
+			HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+			break;
+		case 1:
+			HAL_NVIC_SetPriority(EXTI1_IRQn, 2, 0);
+			HAL_NVIC_EnableIRQ(EXTI1_IRQn);
+			break;
+		case 2:
+			HAL_NVIC_SetPriority(EXTI2_IRQn, 2, 0);
+			HAL_NVIC_EnableIRQ(EXTI2_IRQn);
+			break;
+		case 3:
+			HAL_NVIC_SetPriority(EXTI3_IRQn, 2, 0);
+			HAL_NVIC_EnableIRQ(EXTI3_IRQn);
+			break;
+		case 4:
+			HAL_NVIC_SetPriority(EXTI4_IRQn, 2, 0);
+			HAL_NVIC_EnableIRQ(EXTI4_IRQn);
+			break;
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+			HAL_NVIC_SetPriority(EXTI9_5_IRQn, 2, 0);
+			HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+			break;
+		case 10:
+		case 11:
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+			HAL_NVIC_SetPriority(EXTI15_10_IRQn, 2, 0);
+			HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+			break;
+		}
+	}
+
+}
+
+DigitalIn::~DigitalIn()
+{
+	if (pin_ < 16 && g_interrupt[pin_] == this)
+		g_interrupt[pin_] = 0;
+	HAL_GPIO_DeInit(GPIOx_, 1 << pin_);
+}
+
+void DigitalIn::clock_enable(unsigned int port)
+{
+	switch (port) {
+		case 0:	__GPIOA_CLK_ENABLE(); break;
+		case 1:	__GPIOB_CLK_ENABLE(); break;
+		case 2:	__GPIOC_CLK_ENABLE(); break;
+		case 3:	__GPIOD_CLK_ENABLE(); break;
+		case 4:	__GPIOE_CLK_ENABLE(); break;
+		case 5:	__GPIOF_CLK_ENABLE(); break;
+		case 6:	__GPIOG_CLK_ENABLE(); break;
+		case 7:	__GPIOH_CLK_ENABLE(); break;
+		case 8:	__GPIOJ_CLK_ENABLE(); break;
+		case 9:	__GPIOK_CLK_ENABLE(); break;
+	}
+}
+
+int DigitalIn::read()
+{
+	return (int)HAL_GPIO_ReadPin(GPIOx_, 1 << pin_);
+}
---
+++ cortex-m4/test-printf/application/main.cpp
@@ -0,0 +1,163 @@
+//
+// This file is part of the GNU ARM Eclipse distribution.
+// Copyright (c) 2014 Liviu Ionescu.
+//
+
+// ----------------------------------------------------------------------------
+
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdexcept>
+
+#include "../application/digitalin.h"
+#include "../application/digitalout.h"
+#include "diag/Trace.h"
+#include "stm32f4xx_hal.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+void* __dso_handle = 0;
+
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalIn button(USER_BUTTON, DigitalIn::PullDown, DigitalIn::InterruptRising);
+
+extern "C" void EXTI0_IRQHandler(void)
+{
+	uint32_t mask = portDISABLE_INTERRUPTS();
+	DigitalIn::vector_handler(0);
+	portCLEAR_INTERRUPT_MASK_FROM_ISR(mask);
+}
+
+void main_task(void *pvParameters)
+{
+	uint32_t freq = HAL_RCC_GetSysClockFreq();
+
+	// Infinite loop
+	while (1) {
+		trace_printf("main_task:, freq: %d, f=%f\n", freq, 0.75);
+		vTaskDelay(1000 / portTICK_RATE_MS);
+		try {
+			throw (std::runtime_error("timeout"));
+		} catch (std::exception& e) {
+			trace_printf("main_task:, exception: %s, f=%f\n", e.what(), 0.75);
+		}
+		led1.toggle();
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	uint32_t freq = HAL_RCC_GetSysClockFreq();
+
+	button.callback(&led2, &DigitalOut::toggle);
+
+	  /* Create tasks */
+	xTaskCreate(
+		main_task, /* Function pointer */
+		"Task1", /* Task name - for debugging only*/
+		configMINIMAL_STACK_SIZE, /* Stack depth in words */
+		(void*) NULL, /* Pointer to tasks arguments (parameter) */
+		tskIDLE_PRIORITY + 2UL, /* Task priority*/
+		NULL /* Task handle */
+		);
+
+	xTaskCreate(
+		main_task, /* Function pointer */
+		"Task2", /* Task name - for debugging only*/
+		configMINIMAL_STACK_SIZE, /* Stack depth in words */
+		(void*) NULL, /* Pointer to tasks arguments (parameter) */
+		tskIDLE_PRIORITY + 2UL, /* Task priority*/
+		NULL /* Task handle */
+		);
+
+	xTaskCreate(
+		main_task, /* Function pointer */
+		"Task3", /* Task name - for debugging only*/
+		configMINIMAL_STACK_SIZE, /* Stack depth in words */
+		(void*) NULL, /* Pointer to tasks arguments (parameter) */
+		tskIDLE_PRIORITY + 2UL, /* Task priority*/
+		NULL /* Task handle */
+		);
+
+	vTaskStartScheduler();
+
+	// Infinite loop
+	while (1) {
+		trace_printf("Hello world, freq: %d, f=%f\n", freq, 0.75);
+	}
+	// Infinite loop, never return.
+}
+
+
+extern "C" void vApplicationTickHook(void)
+{
+#if 0
+	{
+		/* Just to verify that the interrupt nesting behaves as expected,
+		 increment ulFPUInterruptNesting on entry, and decrement it on exit. */
+		ulFPUInterruptNesting++;
+
+		/* Fill the FPU registers with 0. */
+		vRegTestClearFlopRegistersToParameterValue( 0UL );
+
+		/* Trigger a timer 2 interrupt, which will fill the registers with a
+		 different value and itself trigger a timer 3 interrupt.  Note that the
+		 timers are not actually used.  The timer 2 and 3 interrupt vectors are
+		 just used for convenience. */
+		NVIC_SetPendingIRQ( TIM2_IRQn );
+
+		/* Ensure that, after returning from the nested interrupts, all the FPU
+		 registers contain the value to which they were set by the tick hook
+		 function. */
+		configASSERT( ulRegTestCheckFlopRegistersContainParameterValue( 0UL ) );
+
+		ulFPUInterruptNesting--;
+	}
+#endif
+}
+
+extern "C" void vApplicationIdleHook(void)
+{
+	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+	 to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+	 task.  It is essential that code added to this hook function never attempts
+	 to block in any way (for example, call xQueueReceive() with a block time
+	 specified, or call vTaskDelay()).  If the application makes use of the
+	 vTaskDelete() API function (as this demo application does) then it is also
+	 important that vApplicationIdleHook() is permitted to return to its calling
+	 function, because it is the responsibility of the idle task to clean up
+	 memory allocated by the kernel to any task that has since been deleted. */
+}
+/*-----------------------------------------------------------*/
+
+extern "C" void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName)
+{
+	(void) pcTaskName;
+	(void) pxTask;
+
+	/* Run time stack overflow checking is performed if
+	 configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+	 function is called if a stack overflow is detected. */
+	taskDISABLE_INTERRUPTS();
+	for (;;)
+		;
+}
+
+extern "C" void vApplicationMallocFailedHook( void )
+{
+    /* vApplicationMallocFailedHook() will only be called if
+    configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+    function that will get called if a call to pvPortMalloc() fails.
+    pvPortMalloc() is called internally by the kernel whenever a task, queue,
+    timer or semaphore is created.  It is also called by various parts of the
+    demo application.  If heap_1.c or heap_2.c are used, then the size of the
+    heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+    FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+    to query the size of free heap space that remains (although it does not
+    provide information on how the remaining heap might be fragmented). */
+    taskDISABLE_INTERRUPTS();
+    for( ;; );
+}
