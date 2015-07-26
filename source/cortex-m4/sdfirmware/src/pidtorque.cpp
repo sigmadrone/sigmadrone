bb6597db6b30fdd15b8da84d2c51c5e4fb20297f
Svetoslav Vassilev <sdvassilev@yahoo.com>
2015-07-25 19:35:30 -0700

    attempt for Z compensation, not tested and disabled

--- source/cortex-m4/sdfirmware/src/pidtorque.cpp
+++ source/cortex-m4/sdfirmware/src/pidtorque.cpp
@@ -25,7 +25,6 @@ void PidTorque::set_target(const QuaternionF &setQ)
 	set_Q_ = setQ;
 }
 
-
 Vector3f PidTorque::get_torque(const QuaternionF &in_Q, const TimeSpan& dt)
 {
 	Vector3f torq;
@@ -34,9 +33,34 @@ Vector3f PidTorque::get_torque(const QuaternionF &in_Q, const TimeSpan& dt)
 	QuaternionF Qtorq = QuaternionF::fromVectors(Zin, Zset);
 	Vector3f error = Qtorq.axis().normalize() * Qtorq.angle() * -1.0;
 
-	last_error_ = error;
+#if 0
+	Vector3f Xset = set_Q_.rotate(Vector3f(1.0, 0.0, 0.0));
+	Vector3f Xin = (~in_Q).rotate(Vector3f(1.0, 0.0, 0.0));
+	QuaternionF QtorqX = QuaternionF::fromVectors(Xin, Xset);
+	Vector3f errorX = QtorqX.axis().normalize() * QtorqX.angle() * -1.0;
+#endif
+
+	// undef the code below to test Z compensation
+#if 0
+	// targetQ = attitudeQ * errQ; ==> (~attitudeQ) * attitudeQ * errQ = (~attitudeQ) * targetQ;
+	// ==> errQ = (~attitudeQ) * targetQ;
+	Qtorq = (~in_Q) * set_Q_;
+	Vector3f error_q = Qtorq.axis().normalize();
+	float angle_rad = Qtorq.angle();
+	if (fabs(angle_rad) > M_PI) {
+		if (angle_rad > 0) {
+			angle_rad -= 2.0 * M_PI;
+		} else {
+			angle_rad += 2.0 * M_PI;
+		}
+	}
+	error_q = error_q * angle_rad;
+	error = error_q;
+#endif
 
 	torq = pid_controller_.get_pid(error,dt.seconds_float());
+
+	last_error_ = error;
 
 	return torq;
 }
