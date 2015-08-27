913a5344f21aa20a3115944600bf2155aa820efe
Martin Stoilov <martin@sigmadrone.org>
2015-08-26 20:06:26 -0700

    Initialize control coefficients

--- source/cortex-m4/sdfirmware/src/pidtorque.cpp
+++ source/cortex-m4/sdfirmware/src/pidtorque.cpp
@@ -61,7 +61,7 @@ Vector3f PidTorque::get_torque(const QuaternionF &in_Q, const TimeSpan& dt)
 	Qtorq = (~in_Q) * set_Q_;
 	QuaternionF swing;
 	QuaternionF::decomposeTwistSwing(Qtorq, Vector3d(0,0,1), swing, twist_);
-	Vector3f error_z = twist_.axis().normalize() * twist_.angle() * -1.0;
+	Vector3f error_z = twist_.axis().normalize() * twist_.angle() * 1.0;
 	error_z.at(0) = error_z.at(1) = 0.0;
 	torq.at(2) = pid_controller_yaw_.get_pid(error_z, dt.seconds_float()).at(2);
 	error.at(2) = error_z.at(2);
