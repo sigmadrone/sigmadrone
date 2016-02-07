646600b03e77082f5d584f9cd2a62e422566a691
Martin Stoilov <martin@sigmadrone.org>
2016-02-06 19:15:58 -0800

    Reduced the default kp.

--- source/cortex-m4/sdfirmware/src/dronestate.h
+++ source/cortex-m4/sdfirmware/src/dronestate.h
@@ -55,7 +55,7 @@ struct DroneState {
 		, course_(-360.0f)
 	    , satellite_count_(0.0f)
 	    , gps_altitude_(Altitude::from_meters(-100))
-		, kp_(0.140 * 3.0)		/* Removed rpm_coef from the calculation and added it here, i.e. 3.0 */
+		, kp_(0.130 * 3.0)		/* Removed rpm_coef from the calculation and added it here, i.e. 3.0 */
 		, ki_(0.060 * 3.0)		/* Removed rpm_coef from the calculation and added it here, i.e. 3.0 */
 		, kd_(0.020 * 3.0)		/* Removed rpm_coef from the calculation and added it here, i.e. 3.0 */
 		, yaw_kp_(0.24)
