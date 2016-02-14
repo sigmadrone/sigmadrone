70d9cd834fcc6927c6a21bb39dabdb0343d4b31e
Martin Stoilov <martin@sigmadrone.org>
2016-02-13 23:01:18 -0800

    reduced yaw zero tolerance offset to 0.0075

--- source/cortex-m4/sdfirmware/src/rcvalueconverter.cpp
+++ source/cortex-m4/sdfirmware/src/rcvalueconverter.cpp
@@ -55,7 +55,7 @@ void RcValueConverter::update()
 	float gear = get_value_as_float(mapper_.channel_no(RC_CHANNEL_ARM_MOTOR));
 
 	throttle_ = Throttle(throttle * scale_factor_);
-	if (yaw > 0.0 && (fabs(yaw-0.5f) > 0.05)) {
+	if (yaw > 0.0 && (fabs(yaw-0.5f) > 0.0075)) {
 		yaw_ = -1 * ((yaw - 0.5) * MAX_EULER_FROM_RC * scale_factor_ * 2);
 		TimeSpan dt = receiver_.channel(mapper_.channel_no(RC_CHANNEL_YAW))->decoder().decoded_period();
 		Vector3f ang_vel(0.0f, 0.0f, yaw_);
