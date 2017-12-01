883e8598cc2ee3a263f7ebda97660a503d52798e
Svetoslav Vassilev <sdvassilev@yahoo.com>
2017-11-30 21:57:37 -0800

    turn on/off collecting/displaying the data, as liveplot with 5-6 axes was rather slow

--- scripts/altitudeplot.py
+++ scripts/altitudeplot.py
@@ -6,26 +6,17 @@ import sys
 from IPython.core.pylabtools import figsize
 import subprocess
 from matplotlib.widgets import RadioButtons
-
-try:
-    f = open(sys.argv[1], "r")
-except Exception as e:
-    print("invalid device file or no device specified")
-    print("example: altitudeplot.py /dev/cu.usbserial-A101J62M")
-    exit(1)
-
-subprocess.Popen(["stty", "-f", sys.argv[1], "230400"])
+import threading
 
 ylim = 3
-sample_count = 2048
 
-xdata = np.linspace(0, sample_count - 1, sample_count)
-altitude = np.linspace(0, 0, sample_count)
-altitude_baro = np.linspace(0, 0, sample_count)
-vert_speed = np.linspace(0, 0, sample_count)
-vert_speed_baro = np.linspace(0, 0, sample_count)
-vert_speed_acc = np.linspace(0, 0, sample_count)
-pause = False
+xdata = []
+altitude = []
+altitude_baro = []
+vert_speed = []
+vert_speed_baro = []
+vert_speed_acc = []
+collect_data = True
 
 
 def setup_axes(axes, ax_label, xlim, ylim):
@@ -38,8 +29,8 @@ def setup_axes(axes, ax_label, xlim, ylim):
 
 # Create a figure with two subplots
 fig, (ax1, ax2) = plt.subplots(2, 1)
-setup_axes(ax1, "Altitude, m", sample_count - 1, ylim)
-setup_axes(ax2, "Vert Speed, m/s", sample_count - 1, ylim)
+setup_axes(ax1, "Altitude, m", 64, ylim)
+setup_axes(ax2, "Vert Speed, m/s", 64, ylim)
 line1, = ax1.plot(xdata, altitude, lw=1, label='Altitude', color='blue')
 line2, = ax1.plot(
     xdata, altitude_baro, lw=1, label='Raw Altitude', color='red')
@@ -54,68 +45,89 @@ ax2.legend(loc='upper left', shadow=True)
 
 plt.subplots_adjust(top=0.8)
 
-
 # initialization function: plot the background of each frame
 def init():
-    line[0].set_data(xdata, altitude)
-    line[1].set_data(xdata, altitude_baro)
-    line[2].set_data(xdata, vert_speed)
-    line[3].set_data(xdata, vert_speed_baro)
-    line[4].set_data(xdata, vert_speed_acc)
-    return line
-
-
-def update_ydata(ydata, new_sample):
-    ydata = np.roll(ydata, -1)
-    ydata[sample_count - 1] = new_sample
-    return ydata
-
-
-# animation function.  This is called sequentially
-def animate(text):
+    global xdata
     global altitude
-    global vert_speed
     global altitude_baro
+    global vert_speed
     global vert_speed_baro
     global vert_speed_acc
-    #print(text)
-    if pause:
-        return line
-    alt, alt_baro, vs, vs_baro, vs_baro_raw, vs_acc = text.split('\n')[
-        0].split(',')[:6]
-    if float(alt) < ax1.get_ylim()[0] or float(alt) > ax1.get_ylim()[1]:
-        ax1.set_ylim([float(alt)-ylim, float(alt)+ylim]) 
 
-    altitude = update_ydata(altitude, alt)
-    altitude_baro = update_ydata(altitude_baro, alt_baro)
-    vert_speed = update_ydata(vert_speed, vs)
-    vert_speed_baro = update_ydata(vert_speed_baro, vs_baro)
-    vert_speed_acc = update_ydata(vert_speed_acc, float(vs_acc) * 10)
-    line[0].set_data(xdata, altitude)
-    line[1].set_data(xdata, altitude_baro)
-    line[2].set_data(xdata, vert_speed)
-    line[3].set_data(xdata, vert_speed_baro)
-    line[4].set_data(xdata, vert_speed_acc)
+    xdata = []
+    altitude = []
+    altitude_baro = []
+    vert_speed = []
+    vert_speed_baro = []
+    vert_speed_acc = []
+    line[0].set_data([],[])
+    line[1].set_data([],[])
+    line[2].set_data([],[])
+    line[3].set_data([],[])
+    line[4].set_data([],[])
     return line
 
+def reader():
+    global collect_data
+    try:
+        f = open(sys.argv[1], "r")
+    except Exception as e:
+        print("invalid device file or no device specified")
+        print("example: altitudeplot.py /dev/cu.usbserial-A101J62M")
+        exit(1)
+    subprocess.Popen(["stty", "-f", sys.argv[1], "230400"])
+    iter = 0
+    while collect_data:
+        iter += 1
+        accumulate_data((iter, f.readline()))
+    
+    f.close()
+
+reader_thread = threading.Thread(target=reader)
+reader_thread.start()
+
+# animation function.  This is called sequentially
+def accumulate_data(indata):
+    t, text = indata
+    if text.count(',') == 5:
+        alt, alt_baro, vs, vs_baro, vs_acc, dt = text.split('\n')[0].split(',')[:6]
+        if len(xdata) == 0:
+            xdata.append(float(dt))
+        else:
+            xdata.append(float(dt) + xdata[len(xdata)-1])
+        altitude.append(float(alt))
+        altitude_baro.append(float(alt_baro))
+        vert_speed.append(float(vs))
+        vert_speed_baro.append(float(vs_baro))
+        vert_speed_acc.append(float(vs_acc) * 10)
+    return
 
 def on_click(label):
-    global pause
-    pause = label == 'Off'
+    global collect_data
+    global reader_thread
+    if label == 'Collect Data':
+        if not collect_data:
+            init()
+            reader_thread = threading.Thread(target=reader)
+            reader_thread.start()
+            collect_data = True
+    else:
+        collect_data = False
+        reader_thread.join()
+        ax1.set_ylim([min(altitude_baro), max(altitude_baro)])
+        ax1.set_xlim([0, xdata[len(xdata)-1]])
+        ax2.set_ylim(min([min(vert_speed_baro),min([min(vert_speed), min(vert_speed_acc)])]), 
+            max([max(vert_speed_baro),max([max(vert_speed), max(vert_speed_acc)])]))
+        ax2.set_xlim([0, xdata[len(xdata)-1]])
+        line[0].set_data(xdata, altitude)
+        line[1].set_data(xdata, altitude_baro)
+        line[2].set_data(xdata, vert_speed)
+        line[3].set_data(xdata, vert_speed_baro)
+        line[4].set_data(xdata, vert_speed_acc)
+        plt.draw()
 
-def datagen():
-    lastline = f.readline()
-    lastline = f.readline()
-    lastline = f.readline()
-    while True:
-        yield f.readline()
-
-rax = plt.axes([0.1, 0.85, 0.11, 0.11])
-radio = RadioButtons(rax, ('On', 'Off'))
+rax = plt.axes([0.1, 0.85, 0.2, 0.15])
+radio = RadioButtons(rax, ('Collect Data', 'Display Data'))
 radio.on_clicked(on_click)
-
-# call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(
-    fig, animate, frames=datagen, init_func=init, interval=1, repeat=True)
 
 plt.show()
